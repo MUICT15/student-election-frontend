daa227a2f30efc07c640a45d0082ddd8a15cdcc6
lagmanzaza <lagmanmc@gmail.com>
2018-04-21 14:47:45 +0700

    [Router] election page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '../pages/index.vue'
-
+import ElectionPage from '../pages/election.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: IndexPage
+    },
+    {
+      path: '/election',
+      name: 'Election',
+      component: ElectionPage
     }
   ]
 })
